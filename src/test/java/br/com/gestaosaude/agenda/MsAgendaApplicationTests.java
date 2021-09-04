f948109c1da2fa904fe91e15311807d89819c127
edicarvalhomeli <edilsoncarvalho@mercadolivre.com>
2021-09-04 15:40:07 -0300

    create project

---
+++ src/test/java/br/com/gestaosaude/agenda/MsAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestaosaude.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
